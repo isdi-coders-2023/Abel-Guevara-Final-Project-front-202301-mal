1736d58b8f21209345db6a3760c7fc0da8f72296
Abel <abelguevaralanda@yahoo.es>
2023-03-20 14:18:26 +0100

    update test

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import { store } from '../../app/store';
 import Login from './Login';
 
 describe('Given a Login pages', () => {
-  test('Render an image', () => {
+  test('Render an input', () => {
     render(
       <Provider store={store}>
-        <Login />
+        <MemoryRouter>
+          <Login />
+        </MemoryRouter>
       </Provider>,
     );
     const input = screen.getByRole('textbox');
