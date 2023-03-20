4cd8aa417ddeda19fe0add4960b5ce7e6454bed2
Abel <abelguevaralanda@yahoo.es>
2023-03-20 11:19:58 +0100

    expand user model

--- src/features/User/UserModel.ts
+++ src/features/User/UserModel.ts
@@ -1,6 +1,13 @@
-interface UserModel {
+type UserModel = {
+  name: string;
+  surname: string;
   email: string;
   password: string;
-}
+};
 export type UserLog = Pick<UserModel, 'email' | 'password'>;
+
+export interface AuthResponse {
+  msg: string;
+  accessToken: string;
+}
 export default UserModel;
