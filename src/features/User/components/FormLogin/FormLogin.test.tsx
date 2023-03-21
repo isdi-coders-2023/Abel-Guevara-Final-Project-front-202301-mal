b7f0d04dce069b73e50215307efa5ae3158f130f
Abel <abelguevaralanda@yahoo.es>
2023-03-21 17:03:05 +0100

    refactizer form login component test

--- src/features/User/components/FormLogin/FormLogin.test.tsx
+++ src/features/User/components/FormLogin/FormLogin.test.tsx
@@ -1,34 +1,20 @@
-import { Provider } from 'react-redux';
-import { store } from '../../../../app/store';
-import { server } from '../../../../mocks/server';
-
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import { handlers } from '../../../../mocks/handlers';
-import userEvent from '@testing-library/user-event';
+import { store } from '../../../../app/store';
+
 import FormLogin from './FormLogin';
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-describe('Given a LoginForm component', () => {
-  describe('When the user logs in with an existing account', () => {
-    test('Dispatches userLogin action on form submission', async () => {
-      server.use(...handlers);
-      render(
-        <Provider store={store}>
-          <MemoryRouter>
-            <FormLogin />
-          </MemoryRouter>
-        </Provider>,
-      );
-      const label = screen.getByLabelText('Dirección de correo electrónico');
-
-      await userEvent.type(label, 'pepito@test.com');
-
-      await userEvent.click(screen.getByRole('button'));
-
-      expect(screen.getByText('Usted ha sido logueado')).toBeInTheDocument();
-    });
+describe('Given a Login pages', () => {
+  test('Render an input', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <FormLogin />
+        </MemoryRouter>
+      </Provider>,
+    );
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
   });
 });
