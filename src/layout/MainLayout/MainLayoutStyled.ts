c0f364ab88c8dd47f76adb513525a54f589371fb
Abel <abelguevaralanda@yahoo.es>
2023-03-24 19:13:54 +0100

    modified main layout

--- src/layout/MainLayout/MainLayoutStyled.ts
+++ src/layout/MainLayout/MainLayoutStyled.ts
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 export const Main = styled.main`
   display: flex;
   justify-content: center;
-  background-image: url('../../../assets/images/home.webp');
-  background-size: cover;
-  background-position: center;
+
   height: 100vh;
 `;
