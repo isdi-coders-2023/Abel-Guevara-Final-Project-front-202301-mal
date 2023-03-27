c730d82de5eabdb697623ec64fe23bfa30eab966
Abel <abelguevaralanda@yahoo.es>
2023-03-27 22:39:47 +0200

    add handlers to test

--- src/pages/Detail/Detail.test.tsx
+++ src/pages/Detail/Detail.test.tsx
@@ -2,7 +2,10 @@ import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { renderWithProviders } from '../../mocks/test-util';
 import Detail from './Detail';
-
+import { server } from '../../mocks/server';
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 describe('Given a Detail page', () => {
   describe('When this page is rendered', () => {
     test('Then should render a img', () => {
