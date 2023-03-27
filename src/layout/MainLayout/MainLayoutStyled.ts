714529f296f9d81c156839e14c29c3e720838215
Abel <abelguevaralanda@yahoo.es>
2023-03-27 09:45:40 +0200

    fix home styles

--- src/layout/MainLayout/MainLayoutStyled.ts
+++ src/layout/MainLayout/MainLayoutStyled.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Main = styled.main`
   display: flex;
   justify-content: center;
-
-  height: 100vh;
+  min-height: 100vh;
+  padding-bottom: 30%;
 `;
