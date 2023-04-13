20188c08a68ffd8a5168eaaeace0b14739af9ebe
Abel <abelguevaralanda@yahoo.es>
2023-04-13 17:08:38 +0200

    add media query desktop

--- src/features/Businesses/components/CardList/CardListStyled.ts
+++ src/features/Businesses/components/CardList/CardListStyled.ts
@@ -5,4 +5,12 @@ export const BusinessesListContainer = styled.ul`
   .business-list {
     width: 100%;
   }
+  @media (min-width: 1440px) {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10%;
+    .business-list {
+      width: 25%;
+    }
+  }
 `;
