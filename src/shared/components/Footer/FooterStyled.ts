bfa44b3758bc2cb3360c8e0bf0a67b6db8518e21
Abel <abelguevaralanda@yahoo.es>
2023-04-15 13:16:10 +0200

    add media query desktop footer

--- src/shared/components/Footer/FooterStyled.ts
+++ src/shared/components/Footer/FooterStyled.ts
@@ -7,9 +7,11 @@ export const FooterContainer = styled.footer`
   align-items: center;
   background: #433e3e;
   bottom: 0px;
-  position: fixed;
   width: 100%;
   height: 15vh;
+  @media (min-width: 768px) {
+    height: 20vh;
+  }
   @media (min-width: 1440px) {
     height: 25vh;
     position: relative;
